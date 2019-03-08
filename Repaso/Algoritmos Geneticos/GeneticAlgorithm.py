58d4036e58992195222f6ad1fdff080af83939f0
David Atehortua <atehortua1907@gmail.com>
2019-03-08 00:23:36 -0500

    Iniciando Funcion de Actitud

---
+++ Repaso/Algoritmos Geneticos/GeneticAlgorithm.py
@@ -0,0 +1,19 @@
+##Problema de la Bolsa (representación binaria)
+
+import numpy as np
+
+def funcion_aptitud(x):
+    peso = np.array([28, 11, 15, 19, 5, 3, 8]) 
+    valor = np.array([40, 22, 30, 37, 8, 5, 12])
+    
+    
+    k = 44 
+    
+    #utilice np.sum para calcular la sumatoria de un vector
+    #COMPLETAR
+    if (None):
+        aptitud = None
+    else:
+        aptitud = None
+    
+    return aptitud
