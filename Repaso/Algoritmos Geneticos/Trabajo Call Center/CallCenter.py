bc1f179df66662dd657a70d254df984f713af2cd
David Atehortua <atehortua1907@gmail.com>
2019-03-25 23:11:08 -0500

    Trabajo de Algoritmo C:/ProgramData/Anaconda3/Scripts/activate

--- Repaso/Algoritmos Geneticos/Trabajo Call Center/CallCenter.py
+++ Repaso/Algoritmos Geneticos/Trabajo Call Center/CallCenter.py
@@ -7,14 +7,16 @@ Characteristics = {
 
 #GA parameters
 numberIterations = 100 #number of iterations
-numberChromosomes = 100 #number of individuals
+numberChromosomes = 10 #number of individuals
 numberGenes = len(Characteristics['Asesores']) #number of genes == cities
 
-pc = 0.9 #Probabilidad de cruce
-pm = 0.5 #Probabilidad de mutación
+crossingProbability = 0.9 #Probabilidad de cruce
+mutationProbability = 0.5 #Probabilidad de mutación
 
 MinValue = 1
 MaxValue = 45
+
+child = None
 
 
 def aptitude_function(chrommosome):
@@ -34,9 +36,74 @@ for i in range(numberChromosomes):
     faAssessor[i] =  aptitude_function(populationAssessor[i])
     faUser[i] =  aptitude_function(populationUser[i])
 
+print('\n-------------Población Inicial Asesor------------- \n', populationAssessor)
+
 #Ciclo principal del algoritmo genético:
 #Seleccion->Cruce->Mutación->Evaluación->Inserción
-# for i in range(numberIterations):
+def ruletaSelection(fa):
 
-print('Población Asesor /n', populationAssessor)
-print('Población Usuario /n', populationUser)
+    totalFa = np.sum([fa], dtype=np.float)
+    percentFa = np.zeros([numberChromosomes], dtype=np.float)
+    selectedCrhomosomes = np.zeros([2], dtype=np.int)
+
+    for i in range(numberChromosomes):
+        percentFa[i] = fa[i]/totalFa
+
+    selectedCrhomosomes[0] = np.argmax(np.cumsum(percentFa) >= np.random.rand())
+    selectedCrhomosomes[1] = np.argmax(np.cumsum(percentFa) >= np.random.rand())
+    
+    return selectedCrhomosomes
+
+
+for i in range(numberIterations):
+
+    selectedCrhomosomes = ruletaSelection(faAssessor)
+    chromoSelectP1 = selectedCrhomosomes[0]
+    chromoSelectP2 = selectedCrhomosomes[1]
+
+    #Cruce Aritmetico Completo
+    if(np.random.rand() <= crossingProbability):
+        a = 0.5
+        fatherA = populationAssessor[chromoSelectP1]
+        fatherB = populationAssessor[chromoSelectP2]
+        child = np.zeros([0], dtype=np.float)
+        for m in range(numberGenes):
+            child = np.append(child,a*fatherA[m]+(1-a)*fatherB[m])
+    
+    if(child is None):
+        continue
+
+    #Mutación, suma número aleatorio a un par de genes seleccionados aleatoriamente (cod. real)
+    if(np.random.rand() <= mutationProbability):
+        randomPosition = np.random.randint(numberGenes)
+        child[randomPosition] =  child[randomPosition] + np.random.uniform(MinValue,MaxValue)/100
+        randomPosition = np.random.randint(numberGenes)
+        child[randomPosition] = child[randomPosition] + np.random.uniform(MinValue,MaxValue)/100
+    
+    #Evaluamos el hijo
+    evaluationChild = aptitude_function(child)
+
+    #Inserción
+    randomPositionFa = np.random.choice([chromoSelectP1, chromoSelectP2])
+    if(evaluationChild > faAssessor[randomPositionFa]):
+        populationAssessor[randomPositionFa] = child
+        faAssessor[randomPositionFa] = evaluationChild    
+
+populationAssessorOrder = np.zeros([numberChromosomes, numberGenes], dtype=np.float)
+count = 0
+
+for i in np.argsort(faAssessor)[::-1]:
+    populationAssessorOrder[count] = populationAssessor[i]
+    count = count + 1
+
+populationUserOrder = np.zeros([numberChromosomes, numberGenes], dtype=np.float)
+count = 0
+for i in np.argsort(faUser):
+    populationUserOrder[count] = populationUser[i]
+    count = count + 1
+
+print('\n------------Población final Asesor-------------\n', populationAssessor)
+print('\n------------Población Usuario----------------\n', populationUser)
+
+for i in range(numberChromosomes):
+    print('\nAl usuario con preferencias ',aptitude_function(populationUserOrder[i]),'Sera atendido por el asesor con caracteristicas ',aptitude_function(populationAssessorOrder[i]))
