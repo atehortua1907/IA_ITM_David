deb080ac529fa1cb78dd9640e2d386a3f8aa8d2b
David Atehortua <atehortua1907@gmail.com>
2019-04-10 23:45:16 -0500

    Inicio Machine Learning

---
+++ Repaso/Machine_Learning/MachineLearningkNN.py
@@ -0,0 +1,9 @@
+'''
+En este notebook se aborda el tema de aprendizaje de máquina para clasificación binario utilizando k-Vecinos Cercanos:
+
+Clasifidores por distancia o cercanía
+Medidas de distancia o similitud
+Clasificación según vecino más próximo
+Clasificación según k vecinos más próximos (k-NN)
+'''
+
