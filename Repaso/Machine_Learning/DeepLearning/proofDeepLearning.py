fa08ef57e7a01c919fa74c72795db4c77f0aeba1
David Atehortua <atehortua1907@gmail.com>
2019-05-28 18:48:12 -0500

    Subiendo archivos taller final deeplearning

---
+++ Repaso/Machine_Learning/DeepLearning/proofDeepLearning.py
@@ -0,0 +1,11 @@
+import os
+import numpy as np
+
+import tensorflow as tf
+
+import keras
+from keras.optimizers import RMSprop
+from keras.preprocessing.image import ImageDataGenerator
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
